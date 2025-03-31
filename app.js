5b64677ab3c9a688bcf518fd0f8339411bfbd038
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-31 11:22:41 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const mongoPass = process.env.MONGO_PASS;
+const mongoUser = process.env.MONGO_USER;
+const mongoAddr = process.env.MONGO_ADDR;
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('67d05ac9e95a8e3a84bf6873')
+        .then((user) => {
+            req.user = user;
+            next();
+        })
+        .catch((err) => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+    .connect(`mongodb+srv://${mongoUser}:${mongoPass}@${mongoAddr}/shop?retryWrites=true`)
+    .then((result) => {
+        User.findOne().then((user) => {
+            if (!user) {
+                const user = new User({
+                    name: 'Alex',
+                    email: 'alex@test.com',
+                    cart: {
+                        items: [],
+                    },
+                });
+                user.save();
+            }
+        });
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
