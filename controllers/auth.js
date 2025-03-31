0be888af468696f1bac9f67e429436c792e18407
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-31 20:37:31 +0300

    247. Two Tiny Improvements

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,14 +20,17 @@ exports.postLogin = (req, res, next) => {
         .then((user) => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            res.redirect('/');
+            req.session.save((err) => {
+                if (err) console.log(err);
+                res.redirect('/');
+            });
         })
         .catch((err) => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy((err) => {
-        console.log(err);
+        if (err) console.log(err);
         res.redirect('/');
     });
 };
